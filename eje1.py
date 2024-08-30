6028ab1b47d098efc8dc65771d59f3e66136e5ad
Claudio curso <acuicolasolmar@gmail.com>
2024-08-30 16:52:48 -0400

    Subiendo el archivo de python.

---
+++ eje1.py
@@ -0,0 +1,9 @@
+print ("Hola le voy hacer una pregunta para conocerlo mejor")
+
+nombre = input ("introduzca su nombre:\n")
+
+edad = input ("introduzca su edad:\n")
+
+pais = input ("introduzca su pais:\n")
+
+print ("hola, su nombre " + nombre + " su edad "  + edad + "su pais " + pais + ".")
